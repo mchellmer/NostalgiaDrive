544efa14236436594515dbfbd63d1ef333b59692
mark <mchellmer@gmail.com>
2018-03-30 16:48:19 +0100

    Added genre and rating column to selection screen

--- nd/ndGui.py
+++ nd/ndGui.py
@@ -37,8 +37,11 @@ class NdGui:
     # Generate selection screen (number of players, genre, rating, popularity)
     def genSelect(self):
         players = ["1", "2", "3", "4"]
+        genres = ["g1", "g2", "g3", "g4"]
 
         self.genChecks(0, players)
+        self.genChecks(1, genres)
+        self.genRating(2)
 
         self.ndMain.mainloop()
 
@@ -49,5 +52,18 @@ class NdGui:
             cButton = Checkbutton(self.ndMain, text=entries[i], variable=CheckVar, onvalue=1, offvalue=0, height=5, width=20)
             cButtons.append(cButton.grid(row=i, column=col))
 
-        for i in range(0, len()):
-            cButtons[i].pack()
+    # TODO: dynamic selection as MIN/MAX can clash
+    def genRating(self, col):
+        Label(self.ndMain, text="Min").grid(row=0, column=col)
+        Label(self.ndMain, text="Max").grid(row=1, column=col)
+
+        a = list(range(11))
+        varMin = IntVar(self.ndMain)
+        varMin.set(0)
+        varMax = IntVar(self.ndMain)
+        varMax.set(10)
+        oMenu = OptionMenu(self.ndMain, varMin, *a)
+        oMenu.grid(row=0, column=col + 1)
+
+        oMenu = OptionMenu(self.ndMain, varMax, *a)
+        oMenu.grid(row=1, column=col + 1)
